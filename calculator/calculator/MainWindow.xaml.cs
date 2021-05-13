91db07def42f1aff985e5573615d509ffec8cab0
korisu07 <Hallo.Dreamy@gmail.com>
2021-05-13 17:46:46 +0900

    Create function and variable.But this is tentative

--- calculator/calculator/MainWindow.xaml.cs
+++ calculator/calculator/MainWindow.xaml.cs
@@ -26,9 +26,25 @@ namespace calculator
             InitializeComponent();
         }
 
-        private String CalcProcess;
+        // 計算の過程を表示する
+        private String ViewerProcess;
 
-        private String CalcResult;
+        // 現在、入力されている最中の数字、
+        // または計算結果の数字を表示する
+        private String ViewerResult;
+
+        // 数字の入力ケタが確定した際に、数字を格納する
+        // 計算記号もここへ
+        private Array CalcProcess;
+
+        private int CalcResult;
+
+        private String[] ArrayCalcProcess;
+
+
+        // すでに計算記号が入力されているかを判別する
+        // trueであれば、入力されている状態
+        private bool BoolEnteredSymbols = false;
 
         // 先頭の括弧がすでに入力されているかを判別する
         // trueであれば、入力されている状態
@@ -39,23 +55,36 @@ namespace calculator
         // 数字を入力するための処理
         private void Button_Click_Number(object sender, RoutedEventArgs e)
         {
-            // 入力された数字を取得
-            String ClickNumber = ((Button)sender).Content.ToString();
+            // 計算記号が入力されているフラグをリセット
+            this.BoolEnteredSymbols = false;
+
+
+            // 入力された数字を取得し、数字に変換
+            String StrEnteredNumber = ((Button)sender).Content.ToString();
+            int ClickNumber = int.Parse( StrEnteredNumber );
 
             // 元々入力されていた数字や計算式と連結させる
-            this.CalcProcess = this.CalcProcess + ClickNumber;
-            this.CalcResult = this.CalcResult + ClickNumber;
+            this.ViewerProcess = this.ViewerProcess + ClickNumber;
+            this.ViewerResult = this.ViewerResult + ClickNumber;
 
             // 入力された値を表示する
             this.ViewEnteredNumber();
 
         }
 
+        private void ConvertArray(String str, RoutedEventArgs e)
+        {
+
+
+
+        }
+  
+
         // 入力された数字や計算式を、画面上に表示する処理
         private void ViewEnteredNumber()
         {
-            TopViewbox.Text = this.CalcProcess;
-            BottomViewbox.Text = this.CalcResult;
+            TopViewbox.Text = this.ViewerProcess;
+            BottomViewbox.Text = this.ViewerResult;
         }
 
         // 計算記号を入力した場合の処理
@@ -65,14 +94,43 @@ namespace calculator
             // 前後に半角スペースを追加
             String ClickSymbols = " " + ((Button)sender).Content.ToString() + " ";
 
-            // 元々入力されていた数字や計算式と連結させる
-            this.CalcProcess = this.CalcProcess + ClickSymbols;
+            // 記号が未入力の場合
+            if( this.BoolEnteredSymbols == false)
+            {
+                this.BoolEnteredSymbols = true;
+
+                // 元々入力されていた数字や計算式と連結させる
+                this.ViewerProcess = this.ViewerProcess + ClickSymbols;
+            } else
+            {
+                
+            }
+
 
             // 入力された値を表示する
             this.ViewEnteredNumber();
 
-            // 前回入力されていた数字をリセット
-            this.CalcResult = null;
+            // 表示し配列に格納後、前回入力されていた数字をリセット
+            this.ViewerResult = null;
+
+        }
+
+        private void ResultCalc(object sender, RoutedEventArgs e)
+        {
+
+            Double d;
+
+             String testStr = "+";
+
+             if( Double.TryParse(testStr, out d) )
+             {
+                 Double.TryParse(testStr, out d);
+                 MessageBox.Show(d.ToString());
+             } else
+             {
+                 MessageBox.Show(testStr);
+             }
+
 
         }
 
