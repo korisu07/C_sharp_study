f8b0f262353fffc6f8df941b59f98f5fa9b58a96
korisu07 <Hallo.Dreamy@gmail.com>
2021-05-17 18:08:07 +0900

    Update and clean functions.

--- calculator/calculator/MainWindow.xaml.cs
+++ calculator/calculator/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
+
 namespace calculator
 {
     /// <summary>
@@ -26,84 +27,149 @@ namespace calculator
             InitializeComponent();
         }
 
-        // 計算の過程を表示する
-        private String ViewerProcess;
+        //
+        // ★表示に関する設定
+        //
 
-        // 現在、入力されている最中の数字、
-        // または計算結果の数字を表示する
-        private String ViewerResult;
+        // 上段に表示する文字列
+        private String ViewerTop = "";
+
+        // 下段に表示する文字列
+        private String ViewerBottom = "";
+
+
+        //
+        // ★内部処理に関する設定
+        //
 
         // 数字の入力ケタが確定した際に、数字を格納する
         // 計算記号もここへ
-        private List ListCalcProcess;
-
-        private int CalcResult;
+        private List<string> ListCalcProcess;
 
 
-        // すでに計算記号が入力されているかを判別する
-        // trueであれば、入力されている状態
-        private bool BoolEnteredSymbols = false;
+        // 現在入力されている数字をリアルタイムで更新する変数
+        private String RealtimeEnterNum;
+
+        // 一時的に入力された計算記号を保存しておく変数
+        private String EnteredCalcSymbols;
+
+        // 計算結果を格納する変数
+        private String CalcResult;
+
+
+        //
+        // ★ON/OFFを判別するための設定
+        //
 
         // 先頭の括弧がすでに入力されているかを判別する
         // trueであれば、入力されている状態
         private bool BoolEnteredbrackets = false;
 
 
+        //
+        // ★関数
+        //
+
+        // 入力された数字や計算式を、画面上に表示する処理
+        // AddStr → 後ろに追加する文字列を代入
+        private void ViewEnteredBtn(String TopStr = "", String BottomStr = "")
+        {
+            this.ViewerTop += TopStr;
+            this.ViewerBottom += BottomStr;
+
+            TopViewbox.Text = this.ViewerTop;
+            BottomViewbox.Text = this.ViewerBottom;
+        }
+
         // 押したボタンに設定されている数字を取得して、
         // 数字ボタンが入力された場合の処理
         private void ClickNumberAction(object sender, RoutedEventArgs e)
         {
-            // 入力された数字を取得し、数字に変換
-            String StrEnteredNumber = ((Button)sender).Content.ToString();
-            int ClickNumber = int.Parse(StrEnteredNumber);
+            // 計算記号がすでに登録されている場合
+            if (this.EnteredCalcSymbols != null)
+            {
+                // 下段の値をリセット
+                this.ViewerBottom = null;
 
-            AddBtnValueToList( ClickNumber );
+                // 計算記号をリストに追加
+                AddBtnValueToList( this.EnteredCalcSymbols );
+                // 計算記号をリセット
+                this.EnteredCalcSymbols = null;
+            }
+
+            // 新たに入力された数字を取得し、文字列に変換
+            String ClickBtnNumber = ((Button)sender).Content.ToString();
+
+            // 入力済みの数字＋新たに入力された数字をくっつける
+            this.RealtimeEnterNum += ClickBtnNumber;
+
+            // 上段と下段に入力中の計算式を表示させる
+
+            // 画面に反映
+            ViewEnteredBtn( ClickBtnNumber, ClickBtnNumber );
+
+        } // end void ClickNumberAction.
+
+
+        // 計算記号が入力された場合の処理
+        private void ClickCalcSymbols(object sender, RoutedEventArgs e)
+        {
+            // すでに数字ボタンが入力されている場合
+            if ( this.RealtimeEnterNum != null )
+            {
+                // 新たに入力されたボタンを取得し、文字列に変換
+                String Symbols = ((Button)sender).Content.ToString();
+                // 計算記号を、変数へ格納する
+                this.EnteredCalcSymbols = Symbols;
+
+                // 画面に反映
+                // 上段にのみ、入力中の計算式を表示させる
+                ViewEnteredBtn(" " + Symbols + " ");
+            } //なにも入力されていない場合
+            else
+            {
+                // なにもしない
+                return;
+            }
 
         }
 
         // 入力された値をListに格納する処理
-        // 数字が入力された場合
-        private static void AddBtnValueToList(int Number)
+        // ★発動タイミング
+        // 数字の場合：次の計算記号が入力された場合
+        // 計算記号の場合：次の数字が入力された場合
+        private void AddBtnValueToList(String Str)
         {
 
-        }
+            // Listを初期化
+            var NewCalcList = new List<string>();
 
-        // 計算記号が入力された場合
-        private static void AddBtnValueToList(String Str)
-        {
+            // ボタンが入力されていない場合
+            if( this.ListCalcProcess == null)
+            {
+                // 新たに入力されたボタンの値をListに追加
+                NewCalcList.Add(Str);
+            }// すでにボタンが入力されている場合 
+            else
+            {
+                // まず、格納されている値をListに追加
+                NewCalcList.AddRange(this.ListCalcProcess);
+                // 新たに入力されたボタンの値を追加
+                NewCalcList.Add( Str );
+            }
 
-        }
-
-
-        private void ConvertArray(String str, RoutedEventArgs e)
-        {
-
-
-
-        }
-  
-
-        // 入力された数字や計算式を、画面上に表示する処理
-        private void ViewEnteredNumber()
-        {
-            //TopViewbox.Text = this.ViewerProcess;
-            //BottomViewbox.Text = this.ViewerResult;
-        }
-
-        // 計算記号が入力された場合の処理
-        private void ClickCalcSymbolsAction(object sender, RoutedEventArgs e)
-        {
-
-
+            this.ListCalcProcess = NewCalcList;
+            
         }
 
         private void ResultCalc(object sender, RoutedEventArgs e)
         {
-
-
-
-
+            // 新たに入力されたボタンを取得し、文字列に変換
+            String Symbols = ((Button)sender).Content.ToString();
+            // それはそれとして「にゃーん」と表示する
+            Console.Write("にゃーん");
         }
+
 
     }
 }
