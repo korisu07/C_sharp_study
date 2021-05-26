b377aebc6b30f192d4b31bc5ab1a2251f0e4c503
korisu07 <Hallo.Dreamy@gmail.com>
2021-05-26 19:16:24 +0900

    Update test file. This is tentative.

--- calculator/CalculatorTest/CalculatorBoolTest.cs
+++ calculator/CalculatorTest/CalculatorBoolTest.cs
@@ -1,15 +1,24 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using CalculatorTest;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using calculator;
 
 
 namespace CalculatorTest
 {
     [TestClass]
-    public class CalculatorBoolTest
+    public class CalculatorBoolTest : MainWindow
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestBoolMethod( bool NumberBool, bool SymbolBool )
         {
+            MainWindow MainClass = new MainWindow();
+
+            Assert.AreEqual<bool>(NumberBool, base.BoolEnteredNumber, "数字に関する例外が発生しました");
+            Assert.AreEqual<bool>(SymbolBool, base.BoolEnteredCalc, "計算記号に関する例外が発生しました");
+
         }
+
     }
 }
